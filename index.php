9f977bb6a1fd7ad076d7e2d7fdb5dde41a370af4
wmilhorini <willymilhorini@gmail.com>
2021-06-19 16:39:18 -0300

    first commit php

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+$categorias = [];
+$categorias[] = 'infantil';
+$categorias[] = 'adolescente';
+$categorias[] = 'adulto';
+$categorias[] = 'idoso';
+//print_r($categorias);
+
+
+$nome = 'Willy';
+$idade = 7;
+
+//var_dump($nome);
+//var_dump($idade);
+
+if($idade >= 6 && $idade <= 12)
+{
+    for($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'infantil')
+            echo " O nadador ".$nome. " compete na categoria " .$categorias[$i];
+    }
+}
+else if($idade >= 13 && $idade <= 18)
+{
+     for($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'adolescente')
+            echo " O nadador ".$nome. " compete na categoria adolescente";
+    }
+}
+else
+{
+     for($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'adulto')
+            echo " O nadador ".$nome. " compete na categoria adulto";
+    }
+}
